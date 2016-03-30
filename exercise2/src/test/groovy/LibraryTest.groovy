ed024f3f61b5a448463c638ed6402e229d1b8f1c
Angel Pimentel <angelpmza@gmail.com>
2016-03-30 00:48:00 -0600

    Add spring boot main class

--- exercise2/src/test/groovy/LibraryTest.groovy
+++ exercise2/src/test/groovy/LibraryTest.groovy
@@ -8,12 +8,5 @@
 import spock.lang.Specification
 
 class LibraryTest extends Specification{
-    def "someLibraryMethod returns true"() {
-        setup:
-        Library lib = new Library()
-        when:
-        def result = lib.someLibraryMethod()
-        then:
-        result == true
-    }
+
 }
